8b2153a0f3cd5c71d8402248c6d8c9d737b94d2f
rawnaq94 <33788325+rawnaq94@users.noreply.github.com>
2019-03-18 00:36:51 +0200

    Create Tree.hpp

---
+++ Tree.hpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+class node
+{
+   	private:
+      	int num;
+	      node *leftChild;
+	      node *rightChild;
+	      node *parent;
+
+	public:
+	node(int i);
+  node();
+	node *getLeftChild();
+	void setLeftChild(node *i);
+	node *getRightChild();
+	void setRightChild(node *j);
+	node *getParent();
+	void setParent(node *k);
+    int getNum();
+};
+
+
+class Tree
+{
+private:
+        node *root;
+        
+ public:
+    Tree();
+    
+    void insert(int i);
+    void remove(int i);
+    static int size();
+    static boolean contains(int i);
+    static int root();
+    static int parent(int i);
+    static int left();
+    static int right();
+    void print();
+    
+};
+
+
