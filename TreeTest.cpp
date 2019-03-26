f78f2f5716d96dbd46f1ec9bb0fa5a0a76991d5f
rawnaq94 <33788325+rawnaq94@users.noreply.github.com>
2019-03-26 14:33:54 +0300

    Update TreeTest.cpp

--- TreeTest.cpp
+++ TreeTest.cpp
@@ -7,6 +7,7 @@
 
 #include <iostream>
 using std::cout, std::endl;
+using namespace std;
 #include "badkan.hpp"
 #include "Tree.hpp"
 
@@ -16,6 +17,41 @@ int main() {
   threetree.insert(5);
   threetree.insert(7);
   threetree.insert(3);
+  
+  // new tree //
+
+  ariel::Tree myTree1;
+  ariel::Tree myTree2;
+  ariel::Tree myTree3;
+  
+  // Fill the first tree //
+
+  myTree1.insert(12);
+  myTree1.insert(6);
+  myTree1.insert(2);
+  
+  
+  // Fill the second tree //
+  
+  myTree2.insert(6);
+  myTree2.insert(2);
+  myTree2.insert(10);
+  
+  // Fill the Third tree //
+  
+  myTree3.insert(10);
+  myTree3.insert(12);
+  myTree3.insert(16);
+  myTree3.insert(11);
+  myTree3.insert(3);
+  myTree3.insert(1);
+  myTree3.insert(7);
+  myTree3.insert(6);
+  myTree3.insert(9);
+  myTree3.insert(8);
+  myTree.print();
+  
+  
   
   badkan::TestCase tc("Binary tree");
   tc
@@ -35,6 +71,29 @@ int main() {
   .CHECK_EQUAL (threetree.right(5), 7)
   .CHECK_THROWS(threetree.insert(3))
   .CHECK_OK    (threetree.print())
+    
+    
+   // myTree1 test //
+   
+   
+  .CHECK_EQUAL(myTree1.size(), 3)
+  .CHECK_EQUAL(myTree1.root(),12)
+  .CHECK_OK   (myTree1.remove(12))
+  .CHECK_EQUAL(myTree1.root(),6)
+  .CHECK_THROWS (myTree1.remove(12))
+  .CHECK_OK   (myTree1.remove(6))
+  .CHECK_EQUAL(myTree1.root(),2)
+  .CHECK_THROWS (myTree1.remove(6))
+  .CHECK_EQUAL(myTree1.size(), 1)
+    
+    
+    // myTree2 test //
+    
+    
+    
+    
+    
+    
   
   .print();
   
