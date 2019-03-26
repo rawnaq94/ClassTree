7f98fb99ad8f3067b611908c6a219be0cfc4bd67
rawnaq94 <33788325+rawnaq94@users.noreply.github.com>
2019-03-26 16:21:45 +0300

    Update Tree.cpp

--- Tree.cpp
+++ Tree.cpp
@@ -1,1 +1,51 @@
 
+#include "Tree.hpp"
+
+namespace ariel{
+    
+
+Tree::Tree(){
+    
+    
+}
+
+Tree Tree::insert(int i){
+    
+    return *this;
+}
+
+Tree Tree::remove(int i){
+    
+    return *this;
+}
+
+int Tree::size(){
+    
+    return 0;
+}
+bool Tree::contains(int i){
+    
+    return false;
+}
+int Tree::root(){
+    
+    return 0;
+}
+int Tree::parent(int i){
+    
+    return 0;
+}
+int Tree::left(int i){
+    
+    return 0;
+}
+int Tree::right(int i){
+    
+    return 0;
+}
+void Tree::print(){
+            
+            
+    
+}
+};
