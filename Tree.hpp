7c3206568ac30eef301a9b179bb6a42f73aa61a9
rawnaq94 <33788325+rawnaq94@users.noreply.github.com>
2019-03-26 14:06:57 +0300

    Update Tree.hpp

--- Tree.hpp
+++ Tree.hpp
@@ -1,50 +1,81 @@
 #include <iostream>
-#include <string>
-
 using namespace std;
 
 
-class node
+class NodeT
 {
    private:
       	int num;
-	      node *leftChild;
-	      node *rightChild;
-	      node *parent;
+	      NodeT* leftChild;
+	      NodeT* rightChild;
+	     
 
    public:
-	node(int i);
-        node();
-	~node();
-	node *getLeftChild();
-	void setLeftChild(node *i);
-	node *getRightChild();
-	void setRightChild(node *j);
-	node *getParent();
-	void setParent(node *k);
-    int getNum();
+	NodeT(int n)
+	{ 
+		num=n; 
+		leftChild=Null;
+		rightChild=Null;
+	} 
+	
+        NodeT();
+	~NodeT();
+	
+	NodeT* getLeftChild() { return leftChild; }
+	void setLeftChild(NodeT* i);
+	{
+		leftChild=i; 
+	}
+	
+	NodeT* getRightChild() { return rightChild; }
+	void setRightChild(NodeT* j)
+	{
+		rightChild=j;
+	}
+	
+        int getNum() { return num; }
+	void setNum(int k) 
+	{
+		num=k;
+	}
 };
+
+namespace ariel {
 
 
 class Tree
 {
-private:
-        node *root;
         
  public:
-    Tree();
-    ~Tree();
-    
-    void insert(int i);
+        Tree()
+        {
+	     root=Null;
+	     cap=0;
+	}
+	
+        ~Tree() 
+	{
+		  destructor(root);
+	}
+	
+    Tree& insert(int i);
     void remove(int i);
-    static int size();
-    static boolean contains(int i);
-    static int root();
-    static int parent(int i);
-    static int left();
-    static int right();
-    void print();
-    
+    int size();
+    boolean contains(int i);
+    int root();
+    int parent(int i);
+    int left();
+    int right();
+    void print(NodeT *root);
+ 
+	
+  private:
+        NodeT *root;
+	int cap;
+	NodeT* byInt(int i);
+        NodeT* max(NodeT *root);
+        void setVer(NodeT *n, NodeT *set, bool right);
+        void destructor(NodeT *root);
 };
-
+}
 
